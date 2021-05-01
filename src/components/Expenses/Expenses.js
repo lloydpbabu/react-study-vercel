39623585547ea3a07519ab53cebe5d8098ec3192
Lloyd <lloydpbabu@gmail.com>
2021-05-02 02:47:07 +0530

    Added filtering for expenses

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -5,7 +5,7 @@ import ExpensesFilter from './ExpensesFilter';
 import { useState } from 'react';
 
 export default function Expenses(props) {
-    const [currentFilter, setcurrentFilter] = useState(2021);
+    const [currentFilter, setcurrentFilter] = useState('2021');
 
 
 
@@ -17,13 +17,11 @@ export default function Expenses(props) {
     return (
         <Card className="expenses">
             <ExpensesFilter onFilterChange={filterHandler} selected={currentFilter} />
-            {props.expenses.map((expense)=>(
-                <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date}/>
+
+            {props.expenses.filter(expense => expense.date.getFullYear().toString() === currentFilter).map((expense) => (
+                <ExpenseItem key={expense.id} title={expense.title} amount={expense.amount} date={expense.date} />
             ))}
-            {/* <ExpenseItem title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date}></ExpenseItem>
-            <ExpenseItem title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date}></ExpenseItem>
-            <ExpenseItem title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date}></ExpenseItem>
-            <ExpenseItem title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date}></ExpenseItem> */}
+
         </Card>
     );
 
