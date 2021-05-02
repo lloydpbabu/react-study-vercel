5c796e056f34977f6e808a7c8b66913a24632699
Lloyd <lloydpbabu@gmail.com>
2021-05-03 00:35:37 +0530

    Added cancel button for new ExpenseForm

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,23 +1,36 @@
+import { useState } from 'react';
 import ExpenseForm from './ExpenseForm';
 import './NewExpense.css';
 
 
-export default function NewExpense(props){
+export default function NewExpense(props) {
 
-    function saveExpenseDataHandler(enteredExpenseData){
+    const [isEditing, setIsEditing] = useState(false);
+
+    function saveExpenseHandler() {
+        setIsEditing(true);
+    }
+
+    function saveExpenseCanceller(){
+        setIsEditing(false);
+    }
+
+    function saveExpenseDataHandler(enteredExpenseData) {
         const expenseData = {
             ...enteredExpenseData,
             id: Math.random().toString()
         };
 
         props.onAddExpenseHandler(expenseData);
+        setIsEditing(false);
     }
 
 
-    return(
-<div className="new-expense">
-<ExpenseForm onSaveExpenseData={saveExpenseDataHandler}/>
-</div>
+    return (
+        <div className="new-expense">
+                {!isEditing && <button onClick={saveExpenseHandler}>Add Expense</button>}
+            {isEditing && <ExpenseForm  onSaveExpenseData={saveExpenseDataHandler} onCancel={saveExpenseCanceller} />}
+        </div>
     );
 
 }
